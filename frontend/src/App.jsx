51b8a22de93331c11f8e0f7708bdf55ee3160b69
Dipen Bhandari <dipenbhandari205@outlook.com>
2026-05-03 19:40:10 -0500

    add backend logic and frontend starter files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import {Route,Routes,BrowserRouter} from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element = {<LandingPage/>}/>
+      </Routes>
+    </BrowserRouter>
+    
+    
+    </>
+  )
+}
+
+export default App
